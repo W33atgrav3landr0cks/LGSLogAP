d92f891e01a9bcbddd0ee435ee5863a12882cad2
RemmelHemmel <jajfilip@gmail.com>
2021-03-04 00:29:25 +0100

    first commit

--- push.bat
+++ push.bat
@@ -3,6 +3,6 @@ git init
 git add *.*
 git commit -m "first commit"
 git branch -M main
-git add remote origin https://github.com/W33atgrav3landr0cks/LGSLogAP.git
+git remote add origin https://github.com/W33atgrav3landr0cks/LGSLogAP.git
 git push -u -f origin main
 pause
